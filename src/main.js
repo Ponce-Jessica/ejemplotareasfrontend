5674ffe3890d2cc45985474126d50d85612e64b0
jessica-ponce <54681651+Ponce-Jessica@users.noreply.github.com>
2022-06-09 17:09:28 -0600

    ok

--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 //url del backend recuerda quitar cualquier pleca del final / quitarla
-axios.defaults.baseURL= process.env.BACKEND || "https://backend.alexandercalderon.me"
-//axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
+axios.defaults.baseURL = process.env.BACKEND || "https://backend.jessicaponce.me"
+    //axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
 
 
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
-  render: h => h(App)
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
